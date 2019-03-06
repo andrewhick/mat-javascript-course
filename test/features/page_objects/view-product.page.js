8290845740482ff07e2ce05949b844036209e33b
simon stratton <student@safebear.co.uk>
2019-03-06 10:42:32 +0000

    Complete page objects

--- test/features/page_objects/view-product.page.js
+++ test/features/page_objects/view-product.page.js
@@ -1,10 +1,13 @@
 /**
- * Page object for CRUD View Product page
- * @constructor
+ * Used to create the locator for the Product element (we don't know what the product name will be)
+ * @param {object} product
+ * @returns {ElementFinder} element
  */
 
  var ViewProductPage = function() {
-
+    this.productName = function(product) {
+        return Element(by.cssContainingText('h2', product.name));
+    };
  };
 
  module.exports = new ViewProductPage();
