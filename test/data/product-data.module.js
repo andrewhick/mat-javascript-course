8e9e1ae46bcdbc817e8c35774b10035b983a650b
simon stratton <student@safebear.co.uk>
2019-03-07 14:40:15 +0000

    Add data into tests

---
+++ test/data/product-data.module.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+    productInfo: {
+        'meat': {
+            name: 'sausages',
+            description: 'thin meat',
+            price: '100'
+        },
+        'vegetables': {
+            name: 'carrots',
+            description: 'orange vegetables',
+            price: '200'
+        },
+        'bread': {
+            name: 'bloomer',
+            description: 'white bread',
+            price: '400'
+        }
+    }
+};
--- test/products/products_crud.spec.js
+++ test/products/products_crud.spec.js
@@ -2,21 +2,31 @@ const homePage = require('../page_objects/home.page');
 const addProductPage = require('../page_objects/add-product.page');
 const viewProductPage = require('../page_objects/view-product.page');
 
-beforeEach(function() {
-    browser.get('');
+// TEST DATA: Import our test module and the 'jasmine data provider' 'using' command to handle our data
+ var using = require("jasmine-data-provider");
+ var products = require("../data/product-data.module.js");
+
+// TEST DATA: Add a 'describe' which encapsulates all tests:
+describe("productTests", function() {
+    beforeEach(function() {
+        browser.get("")
+    })
+
+    // TEST DATA: Add 'using' to use our test data:
+    using(products.productInfo, function(product, description) {
+        it('should create a product ' + description, function() {
+        
+            // TODO: add a check that no product exists here.
+            homePage.addProduct.click();
+        
+            // fill in form:
+            addProductPage.productName.sendKeys(product.name);
+            addProductPage.productDescription.sendKeys(product.description);
+            addProductPage.productPrice.sendKeys(product.price);
+            addProductPage.submitButton.click();
+        
+            // check product name
+            expect(viewProductPage.productName(product).isDisplayed()).toBeTruthy();
+        });
+    });
 });
-
-it('should create a product', function() {
-    
-    homePage.addProduct.click();
-
-    // fill in form:
-    addProductPage.productName.sendKeys("turbot");
-    addProductPage.productDescription.sendKeys("fish");
-    addProductPage.productPrice.sendKeys("100");
-    addProductPage.submitButton.click();
-
-    // check product name
-    expect(viewProductPage.productName({name: "turbot"}).isDisplayed()).toBeTruthy();
-    
-})
